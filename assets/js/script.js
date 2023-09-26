9837b0955bc9a409085b6c5f54abda51f7fac282
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-26 16:44:19 +0100

    wip

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// let text;
+// let number1=5;
+// let number2=3;
+
+// text = number1+ "" + number2
+// alert(text)
+
+let nom = prompt("entrez votre nom");
+let prenom = prompt("entrez votre prenom");
+let message = prompt("entrez votre message");
+let result = `${nom} ${prenom} ${message}`
+alert(result)
