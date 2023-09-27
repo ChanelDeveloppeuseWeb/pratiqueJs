7d9be5b7df9ee0f752c5b619d2ac205e0b73f716
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-27 19:10:24 +0100

    switch case

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+let tiroir = parseFloat(prompt("choissisez le tiroir a ouvrir"))
+switch (tiroir) {
+    case 1:
+        alert("Contient divers outils pour dessiner : du papier, des crayons, etc.")
+        break;
+        case 2:
+        alert("Contient du matériel informatique : des câbles, des composants, etc.")
+        break;
+        case 2:
+        alert("Contient du matériel informatique : des câbles, des composants, etc.")
+        break;
+        case 3:
+        alert("Contient du matériel informatique : des câbles, des composants, etc.")
+        break;
+
+        default:
+        alert('Info du jour : le meuble ne contient que 4 tiroirs et, jusqu \'à preuve du contraire, les tiroirs négatifs n\'existent pas.')
+        break;
+}
