080f92fb499a2e7d52999704d59677686ac65aca
kazi kuete charnel <chanelkuete2@gmail.com>
2023-10-05 15:50:40 +0100

    wip

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+//let navigateur = document.getElementById("navig").innerHTML = navigateur
+
+let text = document.title
+document.getElementById("date").innerHTML = text
+let date = new Date
+document.getElementById("titre").innerHTML = date
+let version = navigator.userAgent
+document.getElementById("navig").innerHTML = version
